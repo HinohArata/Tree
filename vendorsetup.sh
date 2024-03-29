b1aad1409f49300f1eebcb80944b75d6e7390c34
HinohArata <mmgcntk@gmail.com>
2024-03-30 00:40:30 +0700

    Inital commity

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# Clone Vendor Source
+git clone https://github.com/HorizonDroid-Org-Devices/vendor_xiaomi_surya.git vendor/xiaomi/surya
+
+# Clone Kernel Source
+git clone https://github.com/HinohArata/msm-4.14.git -b surya kernel/xiaomi/surya
+
+# Clone Vendor Leica/Miui Camera
+git clone https://gitlab.com/HinohArata/vendor_xiaomi_surya-miuicamera.git -b new vendor/xiaomi/surya-miuicamera
+
+# Clone Device Leica/Miui Camera
+git clone https://gitlab.com/HinohArata/device_xiaomi_surya-miuicamera.git device/xiaomi/surya-miuicamera
+
+# Clone Firmware Images
+git clone https://gitlab.com/HinohArata/firmware_xiaomi_surya.git firmware/xiaomi/surya
